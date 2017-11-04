8709ba424ea740ac37637cd0b86458bda18a0874
Chester Ayala <flippyveteranstorm@icloud.com>
2017-11-05 02:51:57 +0800

    Update corrupt-video-scanner.bat

--- corrupt-video-scanner.bat
+++ corrupt-video-scanner.bat
@@ -1,16 +1,23 @@
-ECHO "SCAN IN PROGRESS - PRESS CTRL + C TO CANCEL THE BATCH PROCESS."
 @ECHO OFF
+ECHO //////////////////////////////////////////////////////////////////
+ECHO SCAN IN PROGRESS - PRESS CTRL + C TO CANCEL THE BATCH PROCESS.
+ECHO //////////////////////////////////////////////////////////////////
+ECHO.
+ECHO A text file will open to show the list of videos that are corrupt.
+
 SET "filtro=%1"
 IF [%filtro%]==[] (
-    SET "filtro=*.mp4"
-    )
+	SET "filtro=*.mp4"
+	)
 
 FOR /R %%a IN (%filtro%) DO CALL :doWork "%%a"
-
-    C:\ffmpeg\corrupt-videos.log
-    EXIT /B
+	DEL "%temp%\corrupt-temp.log"
+	START "" "%temp%\corrupt-videos.log"
+	EXIT /B
 
 :doWork
-    C:\ffmpeg\bin\ffmpeg.exe -v error -i %1 -f null - > "C:\ffmpeg\temp.log" 2>&1
-    FOR /f %%i IN ("C:\ffmpeg\temp.log") DO SET size=%%~zi	
-	if %size% gtr 0 echo %1 is corrupt. >> "C:\ffmpeg\corrupt-videos.log"
+	C:\ffmpeg\bin\ffmpeg.exe -v error -i %1 -f null - > "%temp%\corrupt-temp.log" 2>&1
+	FOR /f %%i IN ("%temp%\corrupt-temp.log") DO SET size=%%~zi	
+	IF [%size%] GTR [0] ( 
+		ECHO %1 is corrupt. >> "%temp%\corrupt-videos.log"
+		)
